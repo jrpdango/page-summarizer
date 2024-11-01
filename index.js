afd3fd60f346dac76336a99657498e4e2456e9e6
jrpdango <72563699+jrpdango@users.noreply.github.com>
2024-11-01 17:19:13 +0800

    Implement json body parser for post endpoint

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express';
+
 const app = express();
+app.use(express.json());
 const port = 3000;
 
 app.post('/', (req, res) => {
-    const url = req.body?.url;
+    const url = req.body.url;
     if(!url) {
         res.status(400).send('Error: POST body must have a "url" property.');
     }
