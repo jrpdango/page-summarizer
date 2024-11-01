669f36bd15cddca7cee6b2f7fbc51df20a485660
jrpdango <72563699+jrpdango@users.noreply.github.com>
2024-11-01 18:02:59 +0800

    Insert into jobs table when url passed

--- db/create-table.sql
+++ db/create-table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE jobs (
     id INTEGER PRIMARY KEY,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    result TEXT NOT NULL,
+    result TEXT,
+    is_completed BOOLEAN NOT NULL,
     is_error BOOLEAN NOT NULL
 );
--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import 'dotenv/config';
+import sqlite3 from 'sqlite3';
 
 const app = express();
 app.use(express.json());
 const port = 3000;
+
+const db = new sqlite3.Database('db/data.db');
 
 app.post('/', (req, res) => {
     const url = req.body.url;
     if(!url) {
         res.status(400).send('Error: POST body must have a "url" property.');
     }
+    db.run('INSERT INTO jobs (is_completed, is_error) VALUES (FALSE, FALSE)');
     res.send(url);
 });
 
