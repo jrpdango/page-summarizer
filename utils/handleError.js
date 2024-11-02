eac86945c72f74a9ea15e26db69733f2489880ce
jrpdango <72563699+jrpdango@users.noreply.github.com>
2024-11-02 15:33:27 +0800

    Make handleError throw an error if message or res is not provided

--- utils/handleError.js
+++ utils/handleError.js
@@ -1,6 +1,10 @@
 import { statusType } from '../constants.js';
 
 export const handleError = ({ message, res, job }) => {
+    if(!message || !res) {
+        throw new Error('message or res must be defined');
+    }
+    
     console.error(`${new Date().toLocaleString()} - ${message}`);
     if(!job) {
         res.status(400).send({
