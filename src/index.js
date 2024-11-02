16ff8e458dad33af0d3c139203e5658859cab96b
jrpdango <72563699+jrpdango@users.noreply.github.com>
2024-11-02 20:35:06 +0800

    Create separate createJobHandler for testing

--- src/index.js
+++ src/index.js
@@ -2,12 +2,8 @@ import express from 'express';
 import 'dotenv/config';
 import sqlite3 from 'sqlite3';
 import puppeteer from 'puppeteer';
-import { summarize } from './services/ai.js';
-import { Job } from './utils/job.js';
-import { statusType } from './constants.js';
-import { handleError } from './utils/handleError.js';
-import { scrapePage } from './utils/scrapePage.js';
 import { getJobHandler } from './handlers/getJobHandler.js';
+import { createJobHandler } from './handlers/createJobHandler.js';
 
 const app = express();
 app.use(express.json());
@@ -17,78 +13,7 @@ const db = new sqlite3.Database('db/data.db');
 const browser = await puppeteer.launch();
 
 app.post('/', async (req, res) => {
-    const url = req.body.url;
-    const job = new Job({ db, url });
-
-    // Check if url hasn't been provided
-    if(!url) {
-        handleError({
-            message: 'POST body must have a "url" property',
-            res,
-            job
-        });
-        return;
-    }
-
-    // Check if url from Lifewire or is invalid
-    try {
-        const urlObj = new URL(url);
-        if(urlObj.hostname !== 'www.lifewire.com') {
-            handleError({
-                message: 'For the purposes of this demo, only Lifewire articles are supported.',
-                res,
-                job
-            });
-            return;
-
-        }
-    } catch (error) {
-        handleError({
-            message: error.message, // "Invalid URL"
-            res,
-            job
-        });
-        return;
-    }
-
-    job.insertToDb({ status: statusType.PENDING });
-    res.send({
-        uuid: job.uuid,
-        url,
-        status: statusType.PENDING
-    });
-    
-    let text;
-    try {
-        text = await scrapePage({ browser, url });
-    } catch (error) {
-        console.error(`Puppeteer error: ${error.message}`);
-        job.update({ 
-            status: statusType.FAILED,
-            errorMessage: 'Failed to retrieve text content'
-        });
-        return;
-    }
-
-    let aiResponse;
-    try {
-        aiResponse = await summarize(text);
-
-    } catch(error) {
-        console.error(`AI service error: ${error.message}`);
-        job.update({
-            status: statusType.FAILED,
-            errorMessage: 'Failed to fetch AI response'
-        });
-        return;
-    }
-
-    job.update({
-        status: statusType.COMPLETED,
-        result: aiResponse.response.text()
-    });
-
-    console.log(`${new Date().toLocaleString()} - Job ${job.uuid} done`);
+    await createJobHandler(req, res, db, browser);
 });
 
 app.get('/', (req, res) => {
