26a8fd4fd884aa391c3ac99108bbb2e051a49bd1
jrpdango <72563699+jrpdango@users.noreply.github.com>
2024-11-02 14:24:16 +0800

    Update handleError to work if job is undefined

--- utils/handleError.js
+++ utils/handleError.js
@@ -1,6 +1,14 @@
 import { statusType } from '../constants.js';
 
 export const handleError = ({ message, res, job }) => {
+    if(!job) {
+        res.status(400).send({
+            error: message,
+            status: statusType.FAILED,
+        });
+        return;
+    }
+
     const uuid = job.insertToDb({
         status: statusType.FAILED,
         errorMessage: message
